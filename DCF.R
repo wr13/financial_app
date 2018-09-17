7f066e379d2c1e4b60bde8c24e6aaa589e5b122a
Will Reynolds <43155828+wr13@users.noreply.github.com>
2018-09-17 11:09:33 -0400

    Update DCF.R

--- DCF.R
+++ DCF.R
@@ -2,7 +2,7 @@
 
 
 
-# get information from quandl for FCFF calulation
+# access Quandl's API 
 Quandl.api_key('Sp7tA8SGPyjyqrzkssRT')
 
 # get SHARADAR information from Quandl
@@ -12,20 +12,35 @@ data_amd <- data.frame(data_amd)
 # add quarters to the tabke
 data_amd$quarter <- quarters(data_amd$reportperiod)
 
-# recent subset
+
+# create subsets
+
+# recent 2018 subset
 recent_subset <- subset(data_amd, data_amd$reportperiod == "2018-06-30")
 recent_subset
 # 2017 subset
 subset_2017 <- subset(data_amd, data_amd$reportperiod == "2017-07-01")
+# 2016 subset
+subset_2016 <- subset(data_amd, data_amd$reportperiod == "2016-06-25")
+# 2015 subset
+subset_2015 <- subset(data_amd, data_amd$reportperiod == "2015-06-27")
+# 2014 subset
+subset_2014 <- subset(data_amd, data_amd$reportperiod == "2014-06-28")
+# 2013 subset
+subset_2013 <- subset(data_amd, data_amd$reportperiod == "2013-06-29")
+
+
+
+
 
 
 # get EBITDA 
 
-ebitda_data <- data_amd$ebitda
-data_ebitda_period <- data_amd[,c("reportperiod","ebit", "quarter")]
-data_ebitda_period
-recent_ebitda <- recent_subset$ebitda
-recent_ebitda
+ebit_data <- data_amd$ebit
+data_ebit_period <- data_amd[,c("reportperiod","ebit", "quarter")]
+data_ebit_period
+recent_ebit <- recent_subset$ebit
+recent_ebit
 # taxes
 taxes_data <- data_amd$taxexp
 recent_taxes_data <- recent_subset$taxexp
